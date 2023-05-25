283f1c0f023e6a116cfcbf68ed0cba3f97b897fc
Edward Zota <zotaedward@gmail.com>
2023-05-25 19:26:48 +0200

    esercizio concluso + tutti i bonus (aggiunta la possibilita di chiamare tutti i seed insieme)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(ProjectSeeder::class);
+        $this->call(TypeSeeder::class);
+        $this->call(TechnologySeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
